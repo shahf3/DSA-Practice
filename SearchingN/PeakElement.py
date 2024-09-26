77b8d2f306d7e09ed6ddd4df5a20db87ad2551fb
faizan ali shah <faizan.shah3@mail.dcu.ie>
2024-09-26 02:48:33 -0700

    Peak element

---
+++ SearchingN/PeakElement.py
@@ -0,0 +1,13 @@
+def findPeakElement(self, nums: List[int]) -> int:
+
+        left, right = 0, len(nums) - 1
+        while left < right:
+
+            mid = left + (right - left) // 2
+
+            if nums[mid] < nums[mid+1]:
+                left = mid + 1
+            elif nums[mid] > nums[mid+1]:
+                right = mid
+            
+        return right
